e3e656b655542c800ee0e712768fd4827ecc82b2
amitmahato98 <shankhakush@gmail.com>
2024-08-06 13:39:21 +0545

    code is added

--- code/index.js
+++ code/index.js
@@ -2,3 +2,11 @@ function show_dropdowns(){
 let dropdown = document.getElementById('dropdowns');
 dropdown.classList.toggle("show");
 }
+
+function opensidebar(){
+    let sidebar = document.getElementById('sidebar');
+    let nav_responsive = document.getElementById('nav-responsive');
+    sidebar.classList.toggle("sidebar");
+    // nav_responsive.classList.toggle("overlay")
+    
+}
