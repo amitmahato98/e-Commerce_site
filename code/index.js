d036ba1f87edcb398603c5bba96fa78ead40a7bd
amitmahato98 <shankhakush@gmail.com>
2024-08-07 21:09:49 +0545

    code is added

--- code/index.js
+++ code/index.js
@@ -3,10 +3,79 @@ let dropdown = document.getElementById('dropdowns');
 dropdown.classList.toggle("show");
 }
 
-function opensidebar(){
-    let sidebar = document.getElementById('sidebar');
-    let nav_responsive = document.getElementById('nav-responsive');
-    // sidebar.classList.toggle("active");
-    // nav_responsive.classList.toggle("overlay")
+// function opensidebar(){
+//     let sidebar = document.getElementById('sidebar');
+//     let nav_responsive = document.getElementById('nav-responsive');
+//     // sidebar.classList.toggle("active");
+//     // nav_responsive.classList.toggle("overlay")
     
+// }
+
+function colhome(){
+    const colhome = document.getElementById('colhome');
+    colhome.classList.toggle('collapses')
 }
+// shopes
+function colshop(){
+    const colshop = document.getElementById('colshop');
+    colshop.classList.toggle('collapses')
+}
+
+function colshoplay(){
+    const colhome = document.getElementById('colshop_layouts');
+    colhome.classList.toggle('collapses')
+}
+
+function colshopproductlay(){
+    const colhome = document.getElementById('colshop_productlayouts');
+    colhome.classList.toggle('collapses')
+}
+
+function colshopcollection(){
+    const colhome = document.getElementById('colshop_collection');
+    colhome.classList.toggle('collapses')
+}
+
+function colshopexplore(){
+    const colhome = document.getElementById('colshop_explore');
+    colhome.classList.toggle('collapses')
+}
+
+// products
+
+function colproduct(){
+    const colhome = document.getElementById('colproduct');
+    colhome.classList.toggle('collapses')
+}
+function colproductlay(){
+    const colhome = document.getElementById('colproduct_productlayout');
+    colhome.classList.toggle('collapses')
+}
+
+
+function colproducttype(){
+    const colhome = document.getElementById('colproduct_producttypes');
+    colhome.classList.toggle('collapses')
+}
+
+
+
+function colproductfeature(){
+    const colhome = document.getElementById('colproduct_productfeature');
+    colhome.classList.toggle('collapses')
+}
+
+
+function colproductexclusivefeature(){
+    const colhome = document.getElementById('colproduct_exclusivefeature');
+    colhome.classList.toggle('collapses')
+}
+
+// pages
+
+
+function colpage(){
+    const colhome = document.getElementById('colpage');
+    colhome.classList.toggle('collapses')
+}
+
